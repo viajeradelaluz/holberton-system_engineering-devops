40aee186fa55b11235a22df6d307f0fe3b3df92c
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-18 20:11:32 -0500

    fix: append lines

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-# Let’s practice using Puppet to make changes to our configuration file. Just as in the previous configuration file task, we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
+# Let’s practice using Puppet to make changes to our configuration file.
+# Just as in the previous configuration file task, we’d like you to set up 
+# your client SSH configuration file so that you can connect to a server without typing a password.
 # Requirements:
 # - Your SSH client configuration must be configured to use the private key ~/.ssh/school
 # - Your SSH client configuration must be configured to refuse to authenticate using a password
 
-class {'ssh::client':
-    ensure => present
-    path => '/etc/ssh/ssh_config',
-    options => {
-    'IdentityFile' => '~/.ssh/school',
-    'PasswordAuthentication' => 'no',
-    },
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+-> exec { 'Append IdentityFile':
+  command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+}
+-> exec { 'Append PasswordAuthentication':
+  command => '/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
