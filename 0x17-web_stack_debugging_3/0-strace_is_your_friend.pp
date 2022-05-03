3987dba591a4d8763576a92ab9c01d8b50c67748
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-03 18:55:01 -0500

    feat: web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Find out why Apache is returning a 500 error.
+# Once you find the issue, fix it and then automate it using Puppet
+
+exec {'fix typo':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+}
