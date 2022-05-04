3608d39f3b4e5228bcca7aa9e4857b3490122068
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-03 19:14:35 -0500

    fix: path added

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Find out why Apache is returning a 500 error.
 # Once you find the issue, fix it and then automate it using Puppet
 
-exec {'fix typo':
+exec { 'fix typo':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
