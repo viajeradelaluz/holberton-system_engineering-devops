bd6c74535031b2fae9d3d21f220ff6a2c9f9aa3a
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-21 19:57:06 -0500

    feat: gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module that, using this REST API, for a given employee ID, returns information
+about his/her TODO list progress.
+    - Use urllib or requests module
+    - The script accepts an integer as a parameter, which is the employee ID
+    - The script displays the employee TODO list in this exact format:
+        - First line: Employee EMPLOYEE_NAME is done with
+          tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+            - EMPLOYEE_NAME: name of the employee
+            - NUMBER_OF_DONE_TASKS: number of completed tasks
+            - TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of
+              completed and non-completed tasks
+        - Second and N next lines display the title of completed tasks:
+        TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+REST API: https://jsonplaceholder.typicode.com/
+"""
+
+from sys import argv
+import requests
+
+
+def main():
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get('{}{}'.format(url, argv[1])).json()
+    todos = requests.get('{}{}/todos'.format(url, argv[1])).json()
+
+    done_task = [task.get('title') for task in todos
+                 if task['completed'] is True]
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(users['name'], len(done_task), len(todos)))
+    for title in done_task:
+        print('\t{}'.format(title))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x15-api/README.md
@@ -0,0 +1,30 @@
+# API
+
+One popular way to expose an application and dataset is to use an API. Often, they are the public facing part of websites and micro-services so that allow outsiders to interact with them – access and modify their data. In this project, you will access employee data via an API to organize and export them to different data structures.
+
+This is a perfect example of a task that is not suited for Bash scripting, so let’s build Python scripts.
+
+## Resources
+
+**Read or watch**
+
+- [What is an API](https://www.webopedia.com/definitions/api/)
+- [What is an API? In English, please](https://www.freecodecamp.org/news/what-is-an-api-in-english-please-b880a3214a82/)
+- [What is a REST API](https://www.sitepoint.com/rest-api/)
+- [Python and REST APIs: Interacting With Web Services](https://realpython.com/api-integration-in-python/)
+- [What are microservices](https://smartbear.com/solutions/microservices/)
+- [PEP8 Python style - having a clean code respecting style guide is really appreciated in the industry](https://peps.python.org/pep-0008/)
+
+## Learning Objectives
+
+- What is an API
+- hat is a REST API
+- What are microservices
+- What is the CSV format
+- What is the JSON format
+- Pythonic Package and module name style
+- Pythonic Class name style
+- Pythonic Variable name style
+- Pythonic Function name style
+- Pythonic Constant name style
+- Significance of CapWords or CamelCase in Python
