e1aef0cbf2aaa996c48af281db7f8bdeb1165134
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-23 21:51:56 -0500

    feat: top ten query

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module solves the following requirements and relies on the file
+test/1-main.py
+
+Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+
+Requirements:
+
+    - Prototype: def top_ten(subreddit)
+    - If not a valid subreddit, print None.
+    - NOTE: Invalid subreddits may return a redirect to search results.
+      Ensure that you are not following redirects.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Prints the titles of the first 10 hot posts of a given subreddit """
+    headers = {'User-Agent': 'viajeradelaluz'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    call = requests.get(url, headers=headers, allow_redirects=False)
+
+    if call.status_code != 200:
+        print('None')
+        return
+
+    posts = call.json().get('data').get('children')
+    for post in posts:
+        print(post.get('data').get('title'))
